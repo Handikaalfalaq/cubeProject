30578be44de883851546def992314bcefa624ddd
handika <handikaalfalaq01@gmail.com>
2023-09-07 06:35:13 +0700

    read data dari firestore

--- App.js
+++ App.js
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
 
-export default function App() {
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB0bJ3M7VOvWqN2FvMtPws2do1jhonrYgE",
+//   authDomain: "cube-dev-4622e.firebaseapp.com",
+//   databaseURL: "https://cube-dev-4622e-default-rtdb.asia-southeast1.firebasedatabase.app",
+//   projectId: "cube-dev-4622e",
+//   storageBucket: "cube-dev-4622e.appspot.com",
+//   messagingSenderId: "427577704256",
+//   appId: "1:427577704256:web:fd3fd9effa978f0d5004a5",
+//   measurementId: "G-80YWW37TR5",
+// };
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBWdrE9n0nWlfy2kEnD9-rwb4u_9FYPDQ4",
+    authDomain: "fir-expo-d9316.firebaseapp.com",
+    projectId: "fir-expo-d9316",
+    storageBucket: "fir-expo-d9316.appspot.com",
+    messagingSenderId: "467087330399",
+    appId: "1:467087330399:web:0c83ea542d0b54d8db9fea"
+    };
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [userAction, setUserAction] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "users"));
+
+        querySnapshot.forEach(async (doc) => {
+          const userData = doc.data();
+          setUsers((prev) => [...prev, userData]);
+
+          const userActionsRef = collection(db, "users", doc.id, "userAction");
+          const userActionsSnapshot = await getDocs(userActionsRef);
+
+          userActionsSnapshot.forEach((userActionDoc) => {
+            const userActionData = userActionDoc.data();
+            setUserAction((prev) => [...prev, userActionData]);
+          });
+        });
+      } catch (error) {
+        console.error("Error fetching data from Firestore: ", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Data users:</Text>
+      <Text>Jumlah Users: {users.length}</Text>
+      <Text>Jumlah User Action: {userAction.length}</Text>
+      <Text>avarage User Action / user: {userAction.length/users.length}</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
+
+export default App;
